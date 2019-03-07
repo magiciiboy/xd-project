4d6368410acd2978e22fc23c428affd10faa064c
Magicii <meoruto@gmail.com>
2019-03-06 18:21:59 -0600

    [UPDATE] Figure 2

--- Fig2BGraph.R
+++ Fig2BGraph.R
@@ -1,3 +1,6 @@
+# Author: Tung Huynh
+# Ref: https://www.rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf
+
 library(ggplot2)
 library(tidyr)
 
@@ -25,11 +28,16 @@ fraction_data %>%
   scale_x_continuous(limits = c(1980,2015), expand = c(0, 0)) +
   scale_y_continuous(limits = c(0,0.3), expand = c(0, 0)) +
   xlab("") +
-  ylab("Fraction of collaborations that are cross-disciplinary") +
+  ylab(expression(paste("Fraction of collaborations \nthat are cross-disciplinary"))) +
   theme(axis.text.x = element_text(color="#000000", size=14, angle=0),
         axis.text.y = element_text(color="#000000", size=14, angle=0),
+        plot.margin = margin(1, 1, 1, 1, "cm"),
         legend.justification=c(0,0), 
-        legend.position=c(0.01,0.6)) +
-  scale_x_continuous(breaks=seq(1980,2015,2), labels=ifelse(axis_x_seq %in% c(1980, 1990, 2000, 2010), axis_x_seq, "")) + 
+        legend.position=c(0.01,0.6),
+        legend.background = element_rect(fill=alpha('white', 1.0))) +
+  scale_x_continuous(breaks=seq(1980,2015,2), 
+                     labels=ifelse(axis_x_seq %in% c(1980, 1990, 2000, 2010), axis_x_seq, "")) + 
   scale_fill_discrete(name="Test")
   
+
+ggsave("./output/Fig2B.png", width = 7.0, height = 4.5, units="in")
